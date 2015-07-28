cc264178eade8a39b2986442cf63da8009aad102
John Baylor <john@anyperk.com>
2015-07-28 15:18:14 -0700

    Add -i parameter to grep for case-insensitive comparison.

--- server.rb
+++ server.rb
@@ -12,7 +12,7 @@ get '/countries' do
   term = params['term']
   puts params
   if term 
-    json `grep #{term} countries.txt`.split("\n").sort.take(10)
+    json `grep -i #{term} countries.txt`.split("\n").sort.take(10)
   else 
     json []
   end
